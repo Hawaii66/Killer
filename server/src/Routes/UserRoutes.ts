b29ca00a791c29ba1bf36d6b14e5ee6ce2c7ab4d
Sebastian Ahlman <hawaiilive@outlook.com>
2022-07-09 00:50:16 +0200

    Standard User routes

--- server/src/Routes/UserRoutes.ts
+++ server/src/Routes/UserRoutes.ts
@@ -1,13 +1,39 @@
 import { Response } from "express";
+import { AccessType, GetUser } from "../Database/User";
 import { AuthedRequest, AuthUser } from "../Functions/AuthUser";
 
 
 const express = require("express");
 const router = express.Router();
 
-router.get("/:id",AuthUser,(req:AuthedRequest,res:Response)=>{
-    console.log(req.userID);
-    res.sendStatus(200);
+router.get("/:id/all",AuthUser, async (req:AuthedRequest,res:Response)=>{
+    if(req.userID !== req.params.id) return res.sendStatus(400);
+
+    const dbUser = await GetUser(req.userID, AccessType.ID);
+
+    res.status(200).json(dbUser);
+});
+
+router.get("/:id/name",AuthUser, async (req:AuthedRequest, res:Response) => {
+    if(req.userID !== req.params.id) return res.sendStatus(400);
+
+    const {forename, lastname} = await GetUser(req.userID, AccessType.ID);
+
+    res.json({
+        name:forename + " " + lastname
+    });
+});
+
+router.get("/:id/stats", AuthUser, async (req:AuthedRequest, res:Response) => {
+    if(req.userID !== req.params.id) return res.sendStatus(400);
+   
+    const {kills, alive, type} = await GetUser(req.userID, AccessType.ID);
+
+    res.json({
+        kills,
+        alive,
+        type
+    });
 });
 
 export default router;
